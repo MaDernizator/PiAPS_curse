926c39d15d59ce71d4e2bcdbee319850b75bc7a6
MaD <mdv_andrey@mail.ru>
2025-06-04 00:50:48 +0400

    попытка пофиксить бд

--- app/config.py
+++ app/config.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "some-super-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost/komuservice")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
