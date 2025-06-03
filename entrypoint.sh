b74a35ef66ce366df50cfc3a46bcf2c506c73ad9
MaD <mdv_andrey@mail.ru>
2025-06-04 01:34:51 +0400

    попытка пофиксить бд4

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,13 @@
 DB_HOST=$(echo $DATABASE_URL | sed -E 's|.*://[^@]+@([^:/]+):([0-9]+)/.*|\1|')
 DB_PORT=$(echo $DATABASE_URL | sed -E 's|.*://[^@]+@([^:/]+):([0-9]+)/.*|\2|')
 
-echo "🔄 Ждём, пока БД ($DB_HOST:$DB_PORT) станет доступна..."
-while ! nc -z $DB_HOST $DB_PORT; do
+echo "🔄 Ждём, пока БД станет доступна..."
+
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
   sleep 1
 done
 
 echo "✅ БД доступна, продолжаем"
-
 flask db upgrade
 flask create-superuser
-
 exec flask run --host=0.0.0.0 --port=5000
