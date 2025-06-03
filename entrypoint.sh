3cd796976a207f7c39f07c20eeeeee482dafaf8d
MaD <mdv_andrey@mail.ru>
2025-06-04 01:39:15 +0400

    попытка пофиксить бд5

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Извлекаем хост и порт из DATABASE_URL
-DB_HOST=$(echo $DATABASE_URL | sed -E 's|.*://[^@]+@([^:/]+):([0-9]+)/.*|\1|')
-DB_PORT=$(echo $DATABASE_URL | sed -E 's|.*://[^@]+@([^:/]+):([0-9]+)/.*|\2|')
-
-echo "🔄 Ждём, пока БД станет доступна..."
-
+echo "🔄 Ждём, пока БД станет доступна ($DB_HOST:$DB_PORT)..."
 while ! nc -z "$DB_HOST" "$DB_PORT"; do
   sleep 1
 done
 
 echo "✅ БД доступна, продолжаем"
+
 flask db upgrade
 flask create-superuser
+
 exec flask run --host=0.0.0.0 --port=5000
